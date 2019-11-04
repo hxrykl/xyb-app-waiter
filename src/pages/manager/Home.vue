53aa81b583f27dbf3f0cd3357e181102ecec6ea7
hxrykl <1574551580@qq.com>
2019-11-04 17:38:40 +0800

    样式更新

--- src/pages/manager/Home.vue
+++ src/pages/manager/Home.vue
@@ -2,13 +2,13 @@
   <div class="home">
     <div class="photo">
       <van-swipe :autoplay="3000" indicator-color="white">
-        <van-swipe-item><img width="100%" height="300px" src="../../assets/images/1.jpeg" alt=""></van-swipe-item>
-        <van-swipe-item><img width="100%" height="300px" src="../../assets/images/2.jpeg" alt=""></van-swipe-item>
-        <van-swipe-item><img width="100%" height="300px" src="../../assets/images/3.jpeg" alt=""></van-swipe-item>
-        <van-swipe-item><img width="100%" height="300px" src="../../assets/images/4.jpeg" alt=""></van-swipe-item>
+        <van-swipe-item><img width="100%" height="300px" src="../../assets/images/1.png" alt=""></van-swipe-item>
+        <van-swipe-item><img width="100%" height="300px" src="../../assets/images/2.png" alt=""></van-swipe-item>
+        <van-swipe-item><img width="100%" height="300px" src="../../assets/images/3.png" alt=""></van-swipe-item>
+        <van-swipe-item><img width="100%" height="300px" src="../../assets/images/4.png" alt=""></van-swipe-item>
       </van-swipe>
-      {{token}}
-      {{info}}
+      <!-- {{token}}
+      {{info}} -->
     </div>
     
   </div>
